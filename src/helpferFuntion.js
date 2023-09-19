794b261dc86ad1addf47126bb0bd00532e8adb8d
chiku1c <92277165+chiku1c@users.noreply.github.com>
2023-09-19 17:34:47 +0530

    Update helpferFuntion.js

--- src/helpferFuntion.js
+++ src/helpferFuntion.js
@@ -1,1 +1,9 @@
-
+export function isPowerofTwo(n)
+    {
+        if (n == 0)
+            return false;
+        if ((n & (~(n - 1))) == n)
+            return true;
+        return false;
+    }
+    
